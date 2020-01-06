a7566148e5ad00ab994d5f336d61a005e93fbb11
evahildenbrand <1eva.hildenbrand@gmail.com>
2020-01-06 22:56:15 +0100

    Added variables

--- weather-leonie/src/main/java/weatherLeonie/ExampleResource.java
+++ weather-leonie/src/main/java/weatherLeonie/ExampleResource.java
@@ -4,9 +4,29 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @Path("/hello")
 public class ExampleResource {
+    String csvFile = "http://www.zamg.ac.at/ogd/";
+    URL url = new URL(csvFile);
+    BufferedReader br = null;
+    InputStreamReader input = null;
+    int lineIndex = 1;
+    String line = "";
+    String splitLine = ";";
+    int town = 1;
+    int tempIndex = 5;
+    int windSpeedIndex = 9;
+    int rainIndex = 12;
+    int sunIndex = 15;
+    String[] weatherArray;
+
+    public ExampleResource() throws MalformedURLException {
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
