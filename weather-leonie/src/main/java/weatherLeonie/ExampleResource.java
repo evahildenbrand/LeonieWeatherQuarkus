2e660c99e351d5c95401700e1eac000c3cce994f
evahildenbrand <1eva.hildenbrand@gmail.com>
2020-01-19 22:51:22 +0100

    Added @Consumes and @Produces

--- weather-leonie/src/main/java/weatherLeonie/ExampleResource.java
+++ weather-leonie/src/main/java/weatherLeonie/ExampleResource.java
@@ -3,8 +3,11 @@ package weatherLeonie;
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.*;
 import java.net.MalformedURLException;
@@ -15,6 +18,8 @@ import java.sql.Connection;
 import java.util.List;
 
 @Path("/weather")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class ExampleResource {
     String csvFile = "http://www.zamg.ac.at/ogd/";
     URL url = new URL(csvFile);
